861bb86889cdc300a7f8b6a0f96145bc5f4c5d72
Daniil Beloglazov <zheltogsan@gmail.com>
2023-05-14 20:57:26 +0500

    Oleja's pursuing player

--- Assets/Resources/Scripts/Oleja.cs
+++ Assets/Resources/Scripts/Oleja.cs
@@ -2,19 +2,85 @@ using UnityEngine;
 
 public class Oleja : MonoBehaviour
 {
-    public float speed = 3.0f;
-    public float obstacleRange = 5.0f;
+    public Transform player;
+    public float calmSpeed = 1.0f;
+    public float runningSpeed = 5.0f;
+    public float obstacleCheckRange = 5.0f;
+    public float obstacleCheckSphereRadius = 0.75f;
+    public float randomTurnMinAngle = -110f;
+    public float randomTurnMaxAngle = 110f;
+    public float fovAngle = 60f;
+    public float fovDistance = 10f;
+
+    private void Update()
+    {
+        if (IsPlayerInFOV() && !IsPlayerBehindObstacle())
+        {
+            PursuePlayer();
+        }
+        else
+        {
+            SearchForPlayer();
+        }
+    }
     
-    private void Update() {
+    private void PursuePlayer()
+    {
+        Debug.Log("I C U");
+        transform.LookAt(player);
+        transform.Translate(0, 0, runningSpeed * Time.deltaTime);
+    }
+
+    private void SearchForPlayer()
+    {
         var t = transform;
-        
-        t.Translate(0, 0, speed * Time.deltaTime);
+        t.Translate(0, 0, calmSpeed * Time.deltaTime);
         var ray = new Ray(t.position, t.forward);
 
-        if (!Physics.SphereCast(ray, 0.75f, out var hit)) return;
-        if (!(hit.distance < obstacleRange)) return;
+        if (!Physics.SphereCast(ray, obstacleCheckSphereRadius, out var hit))
+        {
+            return;
+        }
+
+        if (!(hit.distance < obstacleCheckRange))
+        {
+            return;
+        }
+
+        var turnAngle = Random.Range(randomTurnMinAngle, randomTurnMaxAngle);
+        transform.Rotate(0, turnAngle, 0);
+    }
+
+    private bool IsPlayerInFOV()
+    {
+        var t = transform;
+        var forwardDirection = t.forward;
+        var directionToTarget = player.position - t.position;
+        return Vector3.Angle(forwardDirection, directionToTarget) < fovAngle / 2;
+    }
+
+    private bool IsPlayerBehindObstacle()
+    {
+        var direction = player.position - transform.position;
+        var raycastDistance = direction.magnitude < fovDistance ? direction.magnitude : fovDistance;
+        var ray = new Ray(transform.position, direction.normalized);
+        var hits = Physics.RaycastAll(ray, raycastDistance);
+
+        if (hits.Length == 0)
+        {
+            return true;
+        }
         
-        float angle = Random.Range(-110, 110);
-        transform.Rotate(0, angle, 0);
+        foreach (var hit in hits)
+        {
+            if (!hit.transform.TryGetComponent<Player>(out Player _))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        return false;
     }
 }
