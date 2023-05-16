d435f1e17552283d4b5f19d17530c563c7aa0d2d
Daniil Beloglazov <zheltogsan@gmail.com>
2023-05-16 10:51:04 +0500

    Squatting on Ctrl

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -3,17 +3,22 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 3.0f;
-    public float rotHorSen = 7;
+    public float rotHorSen = 7.0f;
+    public float headMinY = 1.0f;
+    public Transform head;
+    public Rigidbody legs;
 
-    private Rigidbody _rigidbody;
     private Light _flashlight;
     private float _rotY;
+    private float _headMaxY;
     private bool _isFlashlightOn = true;
+    private bool _isSquatting = false;
 
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
         _flashlight = GetComponentInChildren<Light>();
+        _headMaxY = head.localPosition.y;
+        Debug.Log("HEAD ON: " + _headMaxY);
     }
 
     private void Update()
@@ -25,7 +30,7 @@ public class Player : MonoBehaviour
             Vector3.ClampMagnitude(new Vector3(deltaX, 0, deltaZ), speed) * Time.deltaTime
         );
             
-        _rigidbody.MovePosition(_rigidbody.position + movement);
+        legs.MovePosition(legs.position + movement);
         
         _rotY += Input.GetAxis("Mouse X") * rotHorSen;
         transform.localEulerAngles = new Vector3(0, _rotY, 0);
@@ -34,11 +39,37 @@ public class Player : MonoBehaviour
         {
             SwitchFlashlight();
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            if (!_isSquatting)
+            {
+                _isSquatting = true;
+                SitDown();
+            }
+            else
+            {
+                _isSquatting = false;
+                StandUp();
+            }
+        }
     }
 
     private void SwitchFlashlight()
     {
         _isFlashlightOn = !_isFlashlightOn;
         _flashlight.enabled = _isFlashlightOn;
+    }
+
+    private void SitDown()
+    {
+        var headPosition = head.localPosition;
+        head.localPosition = new Vector3(headPosition.x, headMinY, headPosition.z);
+    }
+    
+    private void StandUp()
+    {
+        var headPosition = head.localPosition;
+        head.localPosition = new Vector3(headPosition.x, _headMaxY, headPosition.z);
     }
 }
