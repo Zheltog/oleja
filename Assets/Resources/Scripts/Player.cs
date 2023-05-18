61d248cf4c55964d2932978d32317c8523a42de2
Daniil Beloglazov <zheltogsan@gmail.com>
2023-05-18 12:20:25 +0500

    Added running, fixed squatting

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -2,63 +2,102 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 3.0f;
+    public float squattingSpeed = 1.0f;
+    public float calmSpeed = 2.0f;
+    public float runningSpeed = 10.0f;
     public float rotHorSen = 7.0f;
     public float headMinY = 1.0f;
     public Transform head;
 
     private Rigidbody _rigidbody;
     private Light _flashlight;
-    private float _rotY;
+    private float _currentRotY;
     private float _headMaxY;
-    private bool _isFlashlightOn = true;
+    private bool _isFlashlightOn;
     private bool _isSquatting;
+    private bool _isRunning;
+    private float _currentSpeed;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _flashlight = GetComponentInChildren<Light>();
         _headMaxY = head.transform.localPosition.y;
+        _currentSpeed = calmSpeed;
     }
 
     private void Update()
     {
-        var deltaZ = Input.GetAxis("Vertical") * speed;
-        var deltaX = Input.GetAxis("Horizontal") * speed;
+        var deltaZ = Input.GetAxis("Vertical") * _currentSpeed;
+        var deltaX = Input.GetAxis("Horizontal") * _currentSpeed;
 
         var movement = transform.TransformDirection(
-            Vector3.ClampMagnitude(new Vector3(deltaX, 0, deltaZ), speed) * Time.deltaTime
+            Vector3.ClampMagnitude(new Vector3(deltaX, 0, deltaZ), _currentSpeed) * Time.deltaTime
         );
             
         _rigidbody.MovePosition(_rigidbody.position + movement);
         
-        _rotY += Input.GetAxis("Mouse X") * rotHorSen;
-        transform.localEulerAngles = new Vector3(0, _rotY, 0);
+        _currentRotY += Input.GetAxis("Mouse X") * rotHorSen;
+        transform.localEulerAngles = new Vector3(0, _currentRotY, 0);
 
-        if (Input.GetKeyDown(KeyCode.F))
+        ProcessFlashlightSwitch();
+        ProcessSquatting();
+        ProcessRunning();
+    }
+
+    private void ProcessFlashlightSwitch()
+    {
+        if (!Input.GetKeyDown(KeyCode.F))
         {
-            SwitchFlashlight();
+            return;
         }
+        
+        _isFlashlightOn = !_isFlashlightOn;
+        _flashlight.enabled = _isFlashlightOn;
+    }
 
+    private void ProcessSquatting()
+    {
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            if (!_isSquatting)
+            if (!_isSquatting && !_isRunning)
             {
                 _isSquatting = true;
+                _currentSpeed = squattingSpeed;
                 SitDown();
             }
-            else
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            if (_isSquatting)
             {
                 _isSquatting = false;
+                _currentSpeed = calmSpeed;
                 StandUp();
             }
         }
     }
 
-    private void SwitchFlashlight()
+    private void ProcessRunning()
     {
-        _isFlashlightOn = !_isFlashlightOn;
-        _flashlight.enabled = _isFlashlightOn;
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (!_isRunning && !_isSquatting)
+            {
+                _isRunning = true;
+                _currentSpeed = runningSpeed;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            if (_isRunning)
+            {
+                _isRunning = false;
+                _currentSpeed = calmSpeed;
+            }
+        }
     }
 
     private void SitDown()
