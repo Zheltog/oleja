4f4e058f0e3035be71678706e9fcc0a8339353f5
Daniil Beloglazov <zheltogsan@gmail.com>
2023-05-26 19:27:06 +0500

    Added footsteps sound for room

--- Assets/Resources/Scripts/RoomFinish/Characters/Player.cs
+++ Assets/Resources/Scripts/RoomFinish/Characters/Player.cs
@@ -6,6 +6,7 @@ namespace RoomFinish
     {
         public float speed = 2.0f;
         public float rotHorSen = 7.0f;
+        public AudioSource footsteps;
         
         private Rigidbody _rigidbody;
         private float _currentRotY;
@@ -20,6 +21,18 @@ namespace RoomFinish
         {
             var deltaZ = Input.GetAxis("Vertical") * speed;
             var deltaX = Input.GetAxis("Horizontal") * speed;
+            
+            if (deltaX == 0 && deltaZ == 0)
+            {
+                footsteps.Pause();
+            }
+            else
+            {
+                if (!footsteps.isPlaying)
+                {
+                    footsteps.Play();
+                }
+            }
             
             var movement = transform.TransformDirection(
                 Vector3.ClampMagnitude(new Vector3(deltaX, 0, deltaZ), speed) * Time.deltaTime
